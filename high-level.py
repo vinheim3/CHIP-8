bcf495f9fbde9c2252a83820d08050369edec095
vinheim3 <DanCJia@gmail.com>
2016-01-26 21:51:02 +0000

    Updated high-level language
    operations are now in between operands
    can assign and use an operation in one line
    can do multiplication of 2 V registers
    # (replacement for ') can be used for anything and works as a full
    C-like preprocessor macro
    indents still work
    labels are on their own line now and reference the next line

--- high-level.py
+++ high-level.py
@@ -1,39 +1,118 @@
-import sys
+import sys, copy
 myFile = open(sys.argv[1] + ".c8")
 data = myFile.read().split("\n")
 myFile.close()
 
 keywords = {}
-memVars = {}
 lines = []
 currLine = 0x200
 
+data2 = copy.deepcopy(data)
+for i in data2:
+    if len(i) == 0: continue
+    if i[0] == "#":
+        line = i[1:].split()
+        data.pop(0)
+        for c, j in enumerate(data):
+            data[c] = j.replace(line[0], line[1])
+    if i[0] == "$":
+        line = i[1:].split()
+        data.pop(0)
+
+data2 = []
 for i in data:
+    line = i.split()
+    if len(line) == 5 and line[1] == "=":
+        if line[3] in ["+", "|", "&", "^", "-", "<<", ">>"]:
+            data2.append(" ".join([line[0], "=", line[2]]))
+            data2.append(" ".join([line[0], line[3]+"=", line[4]]))
+        if line[3] == "*":
+            data2.append(" ".join([line[0], "=", line[2]]))
+            for cnt in range(int(line[4])-1):
+                data2.append(" ".join([line[0], "+=", line[2]]))
+    else:
+        data2.append(i)
+data = data2
+
+for i in data:
+    i = i.replace("\t", "")
+    
     if i == "" or i[0] == ":":
+        continue
+
+    if i[-1] == ":":
+        keywords[i[:-1]] = currLine
         continue
     
     line = i.split()
-    
-    if i[0] == "'":
-        memVars[line[0][1:]] = line[1]
-        continue
 
-    if line[-1][0] == "*":
-        keywords[line[-1][1:]] = currLine
-        i = " ".join(line[:-1])
-        
-    if i[:4] == "LD I":
-        lines.append("LD I " + memVars[line[2]])
-        currLine += 2
-        continue
+    if "=" in line:
+        if line[2] == "delay":
+            i = "LD " + line[0] + " DT"
+        elif line[2] == "key":
+            i = "LD " + line[0] + " K"
+        elif line[0] == "delay":
+            i = "LD DT " + line[2]
+        elif line[0] == "sound":
+            i = "LD ST " + line[2]
+        elif len(line) > 3:
+            if line[3] == "font":
+                i = "LD F " + line[2]
+            elif line[3] == "BCD":
+                i = "LD B " + line[2]
+        else:
+            i = "LD " + line[0] + " " + line[2]
+
+    if "==" in line:
+        i = "SNE " + line[1] + " " + line[3]
+
+    if "!=" in line:
+        i = "SE " + line[1] + " " + line[3]
+
+    if "+=" in line:
+        i = "ADD " + line[0] + " " + line[2]
+
+    if "|=" in line:
+        i = "OR " + line[0] + " " + line[2]
+
+    if "&=" in line:
+        i = "AND " + line[0] + " " + line[2]
+
+    if "^=" in line:
+        i = "XOR " + line[0] + " " + line[2]
+
+    if "-=" in line:
+        i = "SUB " + line[0] + " " + line[2]
+
+    if "=-" in line:
+        i = "SUBN " + line[0] + " " + line[2]
+
+    if "<<" in line:
+        cnt = int(line[2])
+        i = (("SHL " + line[0] + "\n") * cnt)[:-1]
+        currLine += 2 * (cnt - 1)
+
+    if ">>" in line:
+        cnt = int(line[2])
+        i = (("SHR " + line[0] + "\n") * cnt)[:-1]
+        currLine += 2 * (cnt - 1)
 
     if line[0] in ("JP", "CALL", "SYS"):
         if line[1][0] == "V":
-            lines.append([line[0] + " " + line[1], line[2][2:]])
+            i = [line[0] + " " + line[1], line[2][2:]]
         else:
-            lines.append([line[0], line[1][2:]])
-        currLine += 2
-        continue
+            i = [line[0], line[1][2:]]
+
+    if "not" in line and "pressed" in line:
+        i = "SKP " + line[1]
+    elif "pressed" in line:
+        i = "SKNP " + line[1]
+
+    if "~=" in line:
+        i = "RND " + line[0] + " " + line[2]
+
+    if "return" in line:
+        i = "RET"
 
     lines.append(i)
     currLine += 2
