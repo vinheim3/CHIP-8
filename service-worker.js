809978380c3ac9471ce5b6d5bedc474f041f1f06
daniel.jianoran <daniel.jianoran@cityfibre.com>
2018-10-02 14:24:50 +0100

    return index.html on request to serve '' url

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,8 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request)
+        caches.match(event.request).then(function(response) {
+            return response || caches.match("/index.html");
+        })
     );
   });
