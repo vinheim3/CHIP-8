428001767aa430719778aa906de42572788812a3
vinheim3 <DanCJia@gmail.com>
2018-10-14 12:46:53 +0100

    try using cache versioning

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
+var CACHE_VERSION = 2;
+var CACHE_NAME = 'daniel-jianoran-v' + CACHE_VERSION;
+
 // Listen for install event, set callback
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open('daniel-jianoran-v1').then(function(cache) {
+        caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll([
                 '/chip8.data',
                 '/chip8.js',
@@ -19,4 +22,22 @@ self.addEventListener('fetch', function(event) {
             return response || caches.match("/index.html");
         })
     );
-  });
+});
+
+self.addEventListener('activate', function(event) {
+    // Active worker won't be treated as activated until promise
+    // resolves successfully.
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.map(function(cacheName) {
+            if (CACHE_NAME !== cacheName) {
+                console.log('Deleting out of date cache:', cacheName);
+                
+                return caches.delete(cacheName);
+            }
+            })
+        );
+        })
+    );
+});
