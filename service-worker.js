dd57281924f79dc66f8ffac90664f1212fae8f16
daniel.jianoran <daniel.jianoran@cityfibre.com>
2018-11-20 12:56:36 +0000

    add icons to cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = 2;
+var CACHE_VERSION = 2.1;
 var CACHE_NAME = 'daniel-jianoran-v' + CACHE_VERSION;
 
 // Listen for install event, set callback
@@ -10,7 +10,15 @@ self.addEventListener('install', function(event) {
                 '/chip8.js',
                 '/chip8.wasm',
                 '/index.html',
-                '/manifest.json'
+                '/manifest.json',
+                '/images/icons/icon-72x72.png',
+                '/images/icons/icon-96x96.png',
+                '/images/icons/icon-128x128.png',
+                '/images/icons/icon-144x144.png',
+                '/images/icons/icon-152x152.png',
+                '/images/icons/icon-192x192.png',
+                '/images/icons/icon-384x384.png',
+                '/images/icons/icon-512x512.png',
             ]);
         })
     );
